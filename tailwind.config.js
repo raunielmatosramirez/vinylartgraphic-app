b2608785a077457931688a2c150c7928abbdb06c
raunielmatosramires <raunielmatos@gmail.com>
2025-10-24 12:24:48 -0400

    fix: correct tailwind v4 configuration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", 
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+};
+
+export default config;
